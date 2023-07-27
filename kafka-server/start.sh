2e4434378f996e12d9c0bc7a3b82cf299650a1c2
Vedant Pandey <vedant@triffy.co>
2023-07-27 17:42:36 +0530

    Add kafka server

---
+++ kafka-server/start.sh
@@ -0,0 +1,5 @@
+# Start kafka server
+KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
+bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties
+bin/kafka-server-start.sh config/kraft/server.properties
+
---
+++ kafka-server/topic.sh
@@ -0,0 +1,2 @@
+# Create a topic
+bin/kafka-topics.sh --create --topic triffy-ondc --bootstrap-server localhost:9092
